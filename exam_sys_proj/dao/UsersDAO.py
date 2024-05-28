a11511fe15eaf9928f81b5caa4f7afc14640fbde
sep6738 <135125132+sep6738@users.noreply.github.com>
2024-05-28 22:03:38 +0800

    Update UsersDAO.py
    加入接口（未实现）

--- exam_sys_proj/dao/UsersDAO.py
+++ exam_sys_proj/dao/UsersDAO.py
@@ -5,3 +5,29 @@ from .base_dao import BaseDAO
 class UsersDAO(BaseDAO):
     def __init__(self, db_util):
         super().__init__(db_util, Users, "users", "userID")
+
+    def QueryPasswordViaEmail(self, entity, email):
+        """
+        根据邮箱查询密码
+        :param entity:
+        :param email:
+        :return:
+        """
+        passwordMD5 = None
+        return passwordMD5
+
+    def InsertVerificationCode(self, entity):
+        """
+        插入验证码
+        :param entity: 
+        :return: 
+        """
+        print()
+    
+    def InsertUserData(self, entity):
+        """
+        插入一条用户数据
+        :param entity: 
+        :return: 
+        """
+        print()
