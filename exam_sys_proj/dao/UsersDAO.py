b09e33d5e11467bba4a863151afbfcbde37e3165
sep6738 <135125132+sep6738@users.noreply.github.com>
2024-05-29 15:22:40 +0800

    Update UsersDAO.py

--- exam_sys_proj/dao/UsersDAO.py
+++ exam_sys_proj/dao/UsersDAO.py
@@ -6,28 +6,26 @@ class UsersDAO(BaseDAO):
     def __init__(self, db_util):
         super().__init__(db_util, Users, "users", "userID")
 
-    def QueryPasswordViaEmail(self, entity, email):
+    def QueryPasswordViaEmail(self, email):
         """
-        根据邮箱查询密码
-        :param entity:
+        根据邮箱查询密码hash(bytes类型)
         :param email:
-        :return:
+        :return:Users的一个对象
         """
-        passwordMD5 = None
-        return passwordMD5
+
+        query = f"SELECT passWord FROM {self.table_name} WHERE email = %s"
+        result = self.execute_query(query, (email,))
+        if result:
+            entity = self.entity_class()
+            entity.passWord = result[0][0]
+            return entity
+        return "noResult"
 
     def InsertVerificationCode(self, entity):
         """
-        插入验证码
-        :param entity: 
-        :return: 
+        插入验证码和验证码过期时间
+        :param entity:
+        :return:
         """
         print()
-    
-    def InsertUserData(self, entity):
-        """
-        插入一条用户数据
-        :param entity: 
-        :return: 
-        """
-        print()
+
