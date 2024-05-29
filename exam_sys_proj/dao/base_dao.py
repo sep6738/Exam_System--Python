5679ec2ea9a945791a99f789d9e785ea4c9a5689
sep6738 <135125132+sep6738@users.noreply.github.com>
2024-05-29 15:15:53 +0800

    Update base_dao.py

--- exam_sys_proj/dao/base_dao.py
+++ exam_sys_proj/dao/base_dao.py
@@ -7,26 +7,34 @@ class BaseDAO:
         self.primary_key = primary_key
 
     # 插入数据，传入orm的一个实例
-    def insert(self, entity):
+    def insert(self, entity, accessProtected = True):
         """
         插入数据，传入orm的一个对象，若表的主键自增建议设置主键为None,函数会返回此次插入的数据的主键
         :param entity:
+        :param accessProtected:是否访问私有属性，若为true则从对象取值时值仅被set方法处理，若为false则会经过set和get方法处理
         :return:new_pk
         """
         # 利用orm进行反射构建column列表存储表的所有属性
-        columns = [attr for attr in dir(entity) if not callable(getattr(entity, attr)) and not attr.startswith("_")]
-        # 获取各属性的值
-        # 对代码["_"+pp for pp in columns]的解释：
-        # 加入_直接取只经过set方法处理的数据，如果不要_那么取到的值会经过set方法和get方法的处理
-        values = [getattr(entity, col) for col in ["_"+pp for pp in columns]]
-        # 构建占位符
-        placeholders = ", ".join(["%s"] * len(columns))
-        # 构建sql
-        query = f"INSERT INTO {self.table_name} ({', '.join(columns)}) VALUES ({placeholders})"
-        # print(query)
-        # 传sql
-        new_pk = self._execute_update(query, values)
-        return new_pk
+        try:
+            columns = [attr for attr in dir(entity) if not callable(getattr(entity, attr)) and not attr.startswith("_")]
+            # 获取各属性的值
+            # 对代码["_"+pp for pp in columns]的解释：
+            # 加入_直接取只经过set方法处理的数据，如果不要_那么取到的值会经过set方法和get方法的处理
+            if accessProtected:
+                values = [getattr(entity, col) for col in ["_"+pp for pp in columns]]
+            else:
+                values = [getattr(entity, col) for col in columns]
+            # 构建占位符
+            placeholders = ", ".join(["%s"] * len(columns))
+            # 构建sql
+            query = f"INSERT INTO {self.table_name} ({', '.join(columns)}) VALUES ({placeholders})"
+            # print(query)
+            # 传sql
+            new_pk = self.execute_update(query, values)
+            return new_pk
+        except Exception as e:
+            print(e)
+            return None
 
     # 根据主键查询
     def query(self, entity, primary_key_value):
@@ -38,7 +46,7 @@ class BaseDAO:
         """
         columns = [attr for attr in dir(entity) if not callable(getattr(entity, attr)) and not attr.startswith("_")]
         query = f"SELECT {', '.join(columns)} FROM {self.table_name} WHERE {self.primary_key} = %s"
-        result = self._execute_query(query, (primary_key_value,))
+        result = self.execute_query(query, (primary_key_value,))
         if result:
             return self._create_entity_from_row(result[0])
         return None
@@ -47,25 +55,35 @@ class BaseDAO:
         """
         根据主键更新数据库一行数据
         :param entity:
-        :return:
+        :return:bool
         """
-        columns = [attr for attr in dir(entity) if not callable(getattr(entity, attr)) and not attr.startswith("_")]
-        values = [getattr(entity, col) for col in ["_"+pp for pp in columns]]
-        set_clause = ", ".join([f"{col} = %s" for col in columns])
-        query = f"UPDATE {self.table_name} SET {set_clause} WHERE {self.primary_key} = %s"
-        values.append(getattr(entity, self.primary_key))
-        self._execute_update(query, values)
+        try:
+            columns = [attr for attr in dir(entity) if not callable(getattr(entity, attr)) and not attr.startswith("_")]
+            values = [getattr(entity, col) for col in ["_"+pp for pp in columns]]
+            set_clause = ", ".join([f"{col} = %s" for col in columns])
+            query = f"UPDATE {self.table_name} SET {set_clause} WHERE {self.primary_key} = %s"
+            values.append(getattr(entity, self.primary_key))
+            self.execute_update(query, values)
+            return True
+        except Exception as e:
+            print(e)
+            return False
 
     def delete(self, primary_key_value):
         """
         根据主键删除数据库一行数据
         :param primary_key_value:
-        :return:
+        :return:bool
         """
-        query = f"DELETE FROM {self.table_name} WHERE {self.primary_key} = %s"
-        self._execute_update(query, (primary_key_value,))
+        try:
+            query = f"DELETE FROM {self.table_name} WHERE {self.primary_key} = %s"
+            self.execute_update(query, (primary_key_value,))
+            return True
+        except Exception as e:
+            print(e)
+            return False
 
-    def _execute_query(self, query, params=None):
+    def execute_query(self, query, params=None):
         conn = self.db_util.get_connection()
         try:
             with conn.cursor() as cursor:
@@ -74,7 +92,7 @@ class BaseDAO:
         finally:
             conn.close()
 
-    def _execute_update(self, query, params=None):
+    def execute_update(self, query, params=None):
         conn = self.db_util.get_connection()
         try:
             with conn.cursor() as cursor:
