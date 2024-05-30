fd4be90b3473be38863cd99fd29d279baac9de9b
q <3447227650@qq.com>
2024-05-30 14:57:22 +0800

    增加一个private目录用于存储私人代码

---
+++ exam_sys_proj/orm/RegistrationCode.py
@@ -0,0 +1,60 @@
+import datetime
+import random
+import string
+
+
+class RegistrationCode:
+    def __init__(self,email=None, verificationCode=None, expirationDate=None):
+        self.email = email
+        self.verificationCode = verificationCode
+        self.expirationDate = expirationDate
+
+    @property
+    def email(self):
+        return self._email
+
+    @email.setter
+    def email(self, value):
+        self._email = value
+
+    @property
+    def verificationCode(self):
+        return self._verificationCode
+
+    @verificationCode.setter
+    def verificationCode(self, value):
+        """
+        检测传入数据是否为只包含数字和小写字母的6位字符串
+        若是则存储，否则自动生成
+        :param value:
+        :return:
+        """
+        characters = string.ascii_lowercase + string.digits
+        if isinstance(value, str):
+            if len(value) == 6 and all(char in characters for char in value):
+                self._verificationCode = value
+            else:
+                self._verificationCode = ''.join(random.choice(characters) for _ in range(6))
+        else:
+            self._verificationCode = ''.join(random.choice(characters) for _ in range(6))
+
+    @property
+    def expirationDate(self):
+        """
+        自动执行+5min操作
+
+        :return:
+        """
+        return self._expirationDate + datetime.timedelta(minutes=5)
+
+    @expirationDate.setter
+    def expirationDate(self, value):
+        """
+        检测传入是否是datetime.datetime类型数据，若是则保留到秒储存，若不是则自动获取当前系统时间
+        :param value:
+        :return:
+        """
+        if isinstance(value, datetime.datetime):
+            self._expirationDate = value.replace(microsecond=0)
+        else:
+            self._expirationDate = datetime.datetime.now().replace(microsecond=0)
