1a9cbb95b2deb3981fb2900619f2658f73033dd2
q <3447227650@qq.com>
2024-06-03 11:25:10 +0800

    添加用于通过教师userid查询到其所教授学科知识点的类方法

--- exam_sys_proj/util/teacherUtils.py
+++ exam_sys_proj/util/teacherUtils.py
@@ -1,5 +1,7 @@
 from ..dao.HomeworkOrExamPoolDAO import HomeworkOrExamPool, HomeworkOrExamPoolDAO
 from ..dao.KnowledgePointsDAO import KnowledgePointsDAO, KnowledgePoints
+from ..dao.HepAndKpMediaterDAO import HepAndKpMediater, HepAndKpMediaterDAO
+from ..dao.TeacherCourseDAO import TeacherCourse, TeacherCourseDAO
 import json
 
 
@@ -27,3 +29,18 @@ class TeacherUtils:
         del question_dict['subject']
         homeworkOrExamPool.question = json.dumps(question_dict)
         hoeppk = homeworkOrExamPoolDao.insert(homeworkOrExamPool)
+
+    @classmethod
+    def queryTeacherSubjectKP(cls, db_util, userID: int):
+        """
+        传入dbutil初始化，传入教师的userID进行查询，返回一个元素是“KnowledgePoints类的orm对象”的list
+        :param db_util:
+        :param userID:
+        :return : 元素是knowledgePoints的orm的list
+        """
+        teacherCourseDAO = TeacherCourseDAO(db_util)
+        knowledgePointsDAO = KnowledgePointsDAO(db_util)
+        subject_list = teacherCourseDAO.querySubjectViaTeacherID(userID)
+        print(subject_list[0].subject)
+        result_list = knowledgePointsDAO.query(value=subject_list[0].subject, column_name="subject",  is_all=True)
+        return result_list
