ea524243b11114580cf676c0fe442987436b38fc
EverlastingYuki <2063331724@qq.com>
2024-06-03 20:39:39 +0800

    完成创建题目功能（选择题，判断题，填空题，问答题） 创建试题导入页面

--- exam_sys_proj/insert.sql
+++ exam_sys_proj/insert.sql
@@ -1,10 +1,10 @@
-insert into homework_or_exam_pool (type, question, answer, courseName, difficultyLevel, isActive) VALUES ('选择题', '{
-"main_content": "1+1=?",
-"questions": ["等于1", "等于2", "等于3", "ABC都不对"],
-"type": "选择题",
-"shuffle": false,
-"score": [5]
-}', '{}', '数学', 1, true);
+-- insert into homework_or_exam_pool (type, question, answer, courseName, difficultyLevel, isActive) VALUES ('选择题', '{
+-- "main_content": "1+1=?",
+-- "questions": ["等于1", "等于2", "等于3", "ABC都不对"],
+-- "type": "选择题",
+-- "shuffle": false,
+-- "score": [5]
+-- }', '{}', '数学', 1, true);
 SET @new_hoep_id = LAST_INSERT_ID();
 insert into knowledge_points (kpName, subject) VALUES ('四则运算', '数学');
 insert into knowledge_points (kpName, subject)
