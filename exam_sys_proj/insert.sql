98a0baba2b3453a8b0526a4ed62de258b7179187
q <3447227650@qq.com>
2024-06-03 11:45:54 +0800

    增强sql的健壮性

--- exam_sys_proj/insert.sql
+++ exam_sys_proj/insert.sql
@@ -1,12 +1,14 @@
-insert into homework_or_exam_pool (hepID, type, question, answer, courseName, difficultyLevel, isActive) VALUES (1, '选择题', '{
+insert into homework_or_exam_pool (type, question, answer, courseName, difficultyLevel, isActive) VALUES ('选择题', '{
 "main_content": "1+1=?",
 "questions": ["等于1", "等于2", "等于3", "ABC都不对"],
 "type": "选择题",
-"shuffle": "False",
+"shuffle": false,
 "score": [5]
 }', '{}', '数学', 1, true);
-insert into knowledge_points (kpID, kpName, subject) VALUES (1, '四则运算', '数学');
-insert into hep_and_kp_mediater (hepID, kpID) VALUES (1, 1);
+SET @new_hoep_id = LAST_INSERT_ID();
+insert into knowledge_points (kpName, subject) VALUES ('四则运算', '数学');
+SET @new_kp_id = LAST_INSERT_ID();
+insert into hep_and_kp_mediater (hepID, kpID) VALUES (@new_hoep_id, @new_kp_id);
 insert into users (userName, passWord, name, roleID, email)
 VALUES ('Homura Akemi', '$2b$08$C2pRyZ4v8WHy1BYml6kw1enOkUMvjCbbdrULL6ZThC92ej4TttYzG', 'Homura Akemi
 ', 1, 'student@test.com');
