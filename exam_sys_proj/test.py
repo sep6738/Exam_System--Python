ccb491b2cf166d39cafe0dc763547994059b6faa
EverlastingYuki <2063331724@qq.com>
2024-06-05 21:05:07 +0800

    修复一个小Bug

--- exam_sys_proj/test.py
+++ exam_sys_proj/test.py
@@ -1,8 +1,13 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def index():
-    return render_template('teacher_base.html')
+# from flask import Flask, render_template
+#
+# app = Flask(__name__)
+#
+#
+# @app.route('/')
+# def index():
+#     return render_template('teacher_base.html')
+d = {}
+d[1] = 2
+l = []
+l.append(d)
+print(d.keys())
