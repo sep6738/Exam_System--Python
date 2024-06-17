2e8a2ee4c0108a279e42096e022b7624c46193ec
q <3447227650@qq.com>
2024-06-17 21:01:38 +0800

    修改

--- exam_sys_proj/dao/HomeworkOrExamPoolDAO.py
+++ exam_sys_proj/dao/HomeworkOrExamPoolDAO.py
@@ -1,7 +1,51 @@
 from ..orm.HomeworkOrExamPool import HomeworkOrExamPool
 from .base_dao import BaseDAO
+import json
 
 
 class HomeworkOrExamPoolDAO(BaseDAO):
     def __init__(self, db_util):
         super().__init__(db_util, HomeworkOrExamPool, "homework_or_exam_pool", "hepID")
+
+
+    def query_whole_paper(self, store_paper: dict):
+        # 解析字典
+        questions_list = []
+        answer_list = []
+        diff_list = []
+        for question_type in store_paper["questions"]:
+            pl = str(tuple(store_paper["questions"][question_type][1:]))
+            # 建立sql并查询
+            query = f"select question, difficultyLevel, answer from homework_or_exam_pool where hepID in {pl} ORDER BY FIELD({'hepID, ' + pl[1:-1]})"
+            result = self.execute_query(query)
+            # 处理结果
+            if store_paper["questions"][question_type][0] > 0:
+                questions_list.append("## " + question_type + f"  (一共{len(store_paper['questions'][question_type])-1}小题，每题{store_paper['questions'][question_type][0]}分，共{store_paper['questions'][question_type][0]*(len(store_paper['questions'][question_type])-1)}分)")
+                if len(store_paper["score"]) < len(store_paper["questions"].keys()):
+                    store_paper["score"].append(store_paper['questions'][question_type][0]*(len(store_paper['questions'][question_type])-1))
+                for q_json in result:
+                    answer_list.append(json.loads(q_json[2]))
+                    diff_list.append(q_json[1])
+                    questions_list.append(json.loads(q_json[0]))
+            else:
+                insert_index = len(questions_list)
+                s = 0
+                for q_json in result:
+                    q_dict = json.loads(q_json[0])
+                    q_dict["main_content"] += f"  ({sum(q_dict['score'])}分)"
+                    questions_list.append(q_dict)
+                    answer_list.append(json.loads(q_json[2]))
+                    diff_list.append(q_json[1])
+                    s += sum(q_dict["score"])
+                questions_list.insert(insert_index, "## " + question_type + f"  (一共{len(result)}小题，共{s}分)")
+                if len(store_paper["score"]) < len(store_paper["questions"].keys()):
+                    store_paper["score"].append(s)
+        result_dict = dict()
+        result_dict["type"] = store_paper["type"]
+        result_dict["score"] = store_paper["score"]
+        result_dict["shuffle"] = store_paper["shuffle"]
+        result_dict["question"] = questions_list
+        result_dict["main_content"] = f"# <center>{store_paper['main_content']}</center>\n"
+        return [result_dict, answer_list, diff_list]
+
+
--- exam_sys_proj/util/teacherUtils.py
+++ exam_sys_proj/util/teacherUtils.py
@@ -2,7 +2,10 @@ from ..dao.HomeworkOrExamPoolDAO import HomeworkOrExamPool, HomeworkOrExamPoolDA
 from ..dao.KnowledgePointsDAO import KnowledgePointsDAO, KnowledgePoints
 from ..dao.HepAndKpMediaterDAO import HepAndKpMediater, HepAndKpMediaterDAO
 from ..dao.TeacherCourseDAO import TeacherCourse, TeacherCourseDAO
+from collections import defaultdict
 import json
+import random
+import time
 
 
 class TeacherUtils:
@@ -105,6 +108,206 @@ class TeacherUtils:
             print("batchInsertQuestions error!")
             return "error"
 
+
+    @classmethod
+    def random_paper(cls, input_dict: dict, db_util):
+        random.seed(time.time())
+
+        def get_diff(min_d, max_d):
+            min_d = eval(min_d)
+            max_d = eval(max_d)
+            if 1 <= min_d <= 3 and 1 <= max_d <= 3:
+                return 1,
+            elif 1 <= min_d <= 3 and 4 <= max_d <= 7:
+                return 1, 2
+            elif 1 <= min_d <= 3 and 8 <= max_d <= 10:
+                return 1, 2, 3
+            elif 4 <= min_d <= 7 and 8 <= max_d <= 10:
+                return 2, 3
+            elif 4 <= min_d <= 7 and 4 <= max_d <= 7:
+                return 2,
+            else:
+                return 3,
+
+        def get_interval(diff, min_d, max_d):
+            set1 = set()
+            if diff == 1:
+                set1 = set(range(1, 4))
+            elif diff == 2:
+                set1 = set(range(4, 8))
+            elif diff == 3:
+                set1 = set(range(8, 11))
+            return list(set1 & set(range(eval(min_d), eval(max_d) + 1)))
+
+        questions_dict = dict()
+        score_ratio = [
+            eval(input_dict['overall_difficulty_easy']) / (
+                    eval(input_dict['overall_difficulty_easy']) + eval(input_dict['overall_difficulty_normal']) + eval(
+                input_dict['overall_difficulty_hard'])),
+            eval(input_dict['overall_difficulty_normal']) / (
+                    eval(input_dict['overall_difficulty_easy']) + eval(input_dict['overall_difficulty_normal']) + eval(
+                input_dict['overall_difficulty_hard'])),
+            eval(input_dict['overall_difficulty_hard']) / (
+                    eval(input_dict['overall_difficulty_easy']) + eval(input_dict['overall_difficulty_normal']) + eval(
+                input_dict['overall_difficulty_hard']))
+        ]
+        questions_dict["选择题"] = input_dict["选择题"].copy()
+        questions_dict["判断题"] = input_dict["判断题"].copy()
+        questions_dict["填空题"] = input_dict["填空题"].copy()
+        questions_dict["主观题"] = input_dict["主观题"].copy()
+        score_dict = {"选择题": 0, "判断题": 0, "填空题": 0, "主观题": 0}
+        all_sum_score = 0
+        for i in questions_dict:
+            sum_score = 0
+            sum_count = 0
+            base_score = questions_dict[i]['score_per_question']
+            if base_score == "0.0":
+                base_score = "4"
+            elif base_score == "0":
+                base_score = "15"
+            base_score = eval(base_score)
+            diff_list = get_diff(questions_dict[i]['difficulty_min'], questions_dict[i]['difficulty_max'])
+            for j in questions_dict[i]["amount_per_knowledge_point"]:
+                sum_score += eval(questions_dict[i]["amount_per_knowledge_point"][j]) * base_score
+                sum_count += eval(questions_dict[i]["amount_per_knowledge_point"][j])
+                score_dict[i] = [diff_list, sum_score, sum_count]
+            all_sum_score += sum_score
+        score_list = sorted(score_dict.items(), key=lambda x: len(x[1][0]))
+        sum_list = list()
+        for i in score_ratio:
+            sum_list.append(i * all_sum_score)
+        # print(sum_list)
+        over_list = [-0.15 * sum_list[0], -0.15 * sum_list[1], -0.15 * sum_list[2]]
+        # print(over_list, "over_list")
+        questions_diff_count_dict = dict()
+        for i in score_list:
+            questions_diff_count_dict[i[0]] = list()
+            temp_sum = sum(sum_list[i[1][0][0] - 1:i[1][0][0] - 1 + len(i[1][0])])
+            for j in i[1][0]:
+                temp_rite = (sum_list[j - 1] / temp_sum)
+                sum_list[j - 1] -= i[1][1] * temp_rite
+                questions_diff_count_dict[i[0]] += [j, i[1][2] * temp_rite]
+            if sum_list[0] < over_list[0]:
+                print("error")
+                return {"status_code": 404, "message": "所设定简单题数量过多，请调高大题的难度范围，或调高简单题所占比例",
+                        "content": None}
+            elif sum_list[1] < over_list[1]:
+                print("error")
+                return {"status_code": 404, "message": "所设定中等题数量过多，请调高大题的难度范围，或调高中等题所占比例",
+                        "content": None}
+            elif sum_list[2] < over_list[2]:
+                print("error")
+                return {"status_code": 404, "message": "所设定困难题数量过多，请调高大题的难度范围，或调高困难题所占比例",
+                        "content": None}
+        del temp_sum, temp_rite
+        all_query_kp_dict = dict()
+        for i in questions_diff_count_dict:
+            final_dict = defaultdict(int)
+            # 舍入规则：小于1一律为1，其余四舍五入，若与总数不符则随机差补
+            for j in range(1, len(questions_diff_count_dict[i]), 2):
+                if questions_diff_count_dict[i][j] < 1:
+                    questions_diff_count_dict[i][j] = 1
+                else:
+                    questions_diff_count_dict[i][j] = round(questions_diff_count_dict[i][j])
+            if sum(questions_diff_count_dict[i][1::2]) > score_dict[i][2]:
+                temp_n = 0
+                temp_n2 = 20
+                while temp_n == 0:
+                    if questions_diff_count_dict[i][random.randrange(1, len(questions_diff_count_dict[i]), 2)] == 1:
+                        pass
+                    else:
+                        questions_diff_count_dict[i][random.randrange(1, len(questions_diff_count_dict[i]), 2)] -= 1
+                        temp_n = 1
+                    if temp_n2 == 0:
+                        temp_n = 1
+                    temp_n2 -= 1
+            elif sum(questions_diff_count_dict[i][1::2]) < score_dict[i][2]:
+                questions_diff_count_dict[i][random.randrange(1, len(questions_diff_count_dict[i]), 2)] += 1
+            for j in range(0, len(questions_diff_count_dict[i]), 2):
+                questions_diff_count_dict[i][j] = get_interval(questions_diff_count_dict[i][j],
+                                                               questions_dict[i]['difficulty_min'],
+                                                               questions_dict[i]['difficulty_max'])
+            diff_count_list = []
+            for j in range(1, len(questions_diff_count_dict[i]), 2):
+                for _ in range(questions_diff_count_dict[i][j]):
+                    diff_count_list.append(random.choice(questions_diff_count_dict[i][j - 1]))
+            for j in diff_count_list:
+                final_dict[j] += 1
+            questions_diff_count_dict[i] = dict(final_dict)
+
+            temp_diff_list = list(questions_diff_count_dict[i].keys())
+            if input_dict["shuffle"]:
+                random.shuffle(temp_diff_list)
+            else:
+                temp_diff_list = sorted(temp_diff_list)
+            temp_dict = dict()
+            for j in temp_diff_list:
+                temp_dict[j] = questions_diff_count_dict[i][j]
+            # 得到难度数量字典
+            questions_diff_count_dict[i] = temp_dict
+            # 得到查询知识点字典
+            query_kp_dict = cls._query_diff_to_kp(i, temp_diff_list)
+            # 检查是否存在某个难度的题目数量为0
+
+            if isinstance(query_kp_dict, dict):
+                pass
+            else:
+                return {"status_code": 404,
+                        "message": f"组卷失败，试题库中难度为{query_kp_dict}的{i}数量为零，若您是老师或管理员请在试题库中添加题目后再重试",
+                        "content": None}
+            all_query_kp_dict[i] = query_kp_dict
+            del temp_dict, temp_diff_list
+        questionID_dict = dict()
+        for question_type in questions_diff_count_dict:
+            # 初始化input_dict
+            for _ in input_dict[question_type]["amount_per_knowledge_point"]:
+                input_dict[question_type]["amount_per_knowledge_point"][_] = int(
+                    input_dict[question_type]["amount_per_knowledge_point"][_])
+            # 使用analysis_diff_to_kp时允许3次失误
+            temp_set = set()
+            questionID_list = []
+            for _ in range(3):
+                questionID_list = cls._analysis_diff_to_kp(questions_diff_count_dict[question_type],
+                                                           input_dict[question_type]["amount_per_knowledge_point"],
+                                                           all_query_kp_dict[question_type])
+                if isinstance(questionID_list, list):
+                    break
+                else:
+                    temp_set = temp_set | questionID_list
+                if _ == 2:
+                    if len(temp_set) > 1:
+                        return {"status_code": 404,
+                                "message": f"组卷失败，试题库中知识点为{'、'.join(list(temp_set))}的{question_type}数量不足，若您是老师或管理员请在试题库中添加题目后再重试",
+                                "content": None}
+                    else:
+                        return {"status_code": 404,
+                                "message": f"组卷失败，试题库中知识点为{temp_set[0]}的{question_type}数量不足，若您是老师或管理员请在试题库中添加题目后再重试",
+                                "content": None}
+            questionID_list.insert(0, eval(questions_dict[question_type]['score_per_question']))
+            questionID_dict[question_type] = questionID_list
+        stored_paper_dict = dict()
+        stored_paper_dict["type"] = input_dict["type"]
+        stored_paper_dict["score"] = []
+        stored_paper_dict["shuffle"] = input_dict["shuffle"]
+        stored_paper_dict["main_content"] = input_dict["title"]
+        stored_paper_dict["questions"] = questionID_dict
+        try:
+            homeworkOrExamPoolDAO = HomeworkOrExamPoolDAO(db_util)
+            result = homeworkOrExamPoolDAO.query_whole_paper(stored_paper_dict)
+            stored_paper_dict["score"] = result[0]["score"]
+
+        #     可能后续会有功能拓展
+
+        except Exception as e:
+            print(e)
+            return {"status_code": 404,
+                    "message": "组卷失败，发生未知错误",
+                    "content": None}
+
+        return {"status_code": 200,
+                "message": "组卷成功",
+                "content": [result[0], stored_paper_dict, None, None]}
+
     # @classmethod
     # def batchInsertQuestionsTest(cls, db_util, json_path: str):
     #     """
@@ -189,3 +392,108 @@ class TeacherUtils:
         with open(json_path, "r", encoding="utf-8") as f:
             t = json.load(f)
         return t
+
+
+    @staticmethod
+    def _query_diff_to_kp(type: str, diff_level: list, db_util):
+        homeworkOrExamPoolDAO = HomeworkOrExamPoolDAO(db_util)
+        values = (type,)
+        placeholders = []
+        results_dict = dict()
+        for i in diff_level:
+            placeholders.append("difficultyLevel = " + str(i))
+            results_dict[i] = list()
+        if len(placeholders) > 1:
+            placeholders = " or ".join(placeholders)
+        else:
+            placeholders = placeholders[0]
+        query = f"select difficultyLevel, hep_and_kp_mediater.hepID, kpName from homework_or_exam_pool, hep_and_kp_mediater where type = %s and homework_or_exam_pool.hepID = hep_and_kp_mediater.hepID and ({placeholders}) order by difficultyLevel"
+        results = homeworkOrExamPoolDAO.execute_query(query, values)
+        temp_n = 0
+        while temp_n < len(results) - 1:
+            temp_n2 = 0
+            n2 = results[temp_n][0]
+            while n2 == results[temp_n][0] and temp_n < len(results) - 1:
+                n = results[temp_n][1]
+                results_dict[results[temp_n][0]].append(list())
+                results_dict[results[temp_n][0]][temp_n2].append(n)
+                while results[temp_n][1] == n:
+                    results_dict[results[temp_n][0]][temp_n2].append(results[temp_n][2])
+                    temp_n += 1
+                    if temp_n > len(results) - 1:
+                        temp_n -= 1
+                        break
+                temp_n2 += 1
+        # 检查是否存在某个难度的题目数量为0
+        for i in results_dict:
+            if len(results_dict[i]) == 0:
+                return i
+            # else:
+            #     random.shuffle(results_dict[i])
+        return results_dict
+
+    @staticmethod
+    def _analysis_diff_to_kp(diff_dict: dict, kp_dict: dict, query_dict: dict):
+        random.seed(1234)
+        result_list = list()
+        executing_flag = 0
+
+        for diff in diff_dict:
+            random.shuffle(query_dict[diff])
+
+            # 初始化
+            kp_list = list(kp_dict.keys())
+            random.shuffle(kp_list)
+            kp_name_index = random.choice(list(range(len(kp_list))))
+            temp_kp_name_index = []
+
+            while True:
+                # 循环停止的条件：该难度的题目数量已被消耗完，或者所有非零知识点都轮完
+                del_index, find_flag, break_flag = [], 0, 0
+                for query_index in range(len(query_dict[diff])):
+                    # 还需处理最后一次出现的异常
+                    if len(kp_list) == 0:
+                        return kp_dict.keys()
+                    # 开始寻找
+                    if kp_list[kp_name_index] in query_dict[diff][query_index][0:]:
+                        kp_dict[kp_list[kp_name_index]] -= 1
+                        diff_dict[diff] -= 1
+                        del_index.append(query_index)
+                        result_list.append(query_dict[diff][query_index][0])
+                        if kp_dict[kp_list[kp_name_index]] == 0 and diff_dict[diff] == 0:
+                            del kp_dict[kp_list[kp_name_index]]
+                            break_flag = 1  # 循环可以停止了
+                            break
+                        elif kp_dict[kp_list[kp_name_index]] == 0 and diff_dict[diff] > 0:
+                            temp_kp_name_index.append(kp_list[kp_name_index])
+                            del kp_dict[kp_list[kp_name_index]]  # 此时应该轮换到下一个知识点
+                            del kp_list[kp_name_index]
+                            if len(kp_list) > 0:
+                                kp_name_index = random.choice(list(range(len(kp_list))))
+                            break
+                        elif kp_dict[kp_list[kp_name_index]] > 0 and diff_dict[diff] == 0:
+                            break_flag = 1  # 循环可以停止了
+                            break
+                        find_flag += 1
+                    else:
+                        find_flag += 1
+
+                # 检查是否应该剔除查询结果：
+                if len(del_index) > 0:
+                    m = 0
+                    for _ in del_index:
+                        del query_dict[diff][_ - m]
+                        m += 1
+
+                # 检查是否可以break
+                if break_flag == 1:
+                    break
+
+                # 此时该难度题目任有余，但是却没有与知识点匹配的查询结果，将此次的知识点冻结，换到下一个知识点
+                if find_flag == len(query_dict[diff]) and kp_dict[kp_list[kp_name_index]] > 0:
+                    temp_kp_name_index.append(kp_list[kp_name_index])
+                    del kp_list[kp_name_index]
+                    if len(kp_list) > 0:
+                        kp_name_index = random.choice(list(range(len(kp_list))))
+
+        return result_list
